52cfa8ce5a8b083134779f0eec4836d890e64e77
Oyelabi Paul <59335237+Yodeman@users.noreply.github.com>
2021-06-07 13:05:45 +0100

    update file

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,11 +33,11 @@ def run_the_app():
     uploaded_file = st.file_uploader("Upload an image file...", type=[".png", ".jpg"])
     if uploaded_file:
         img = Image.open(uploaded_file)
-        img.save("app/object_detection/images/test.jpg")
+        img.save("/app/object_detection/images/test.jpg")
         img.close()
-        command = "python detect.py --source app/object_detection/images/test.jpg --weights runs/train/cow_det7/weights/best.pt --conf 0.2 --name output --img-size 600"
+        command = "python detect.py --source /app/object_detection/images/test.jpg --weights runs/train/cow_det7/weights/best.pt --conf 0.2 --name output --img-size 600"
         subprocess.call(command, shell=True)
-        img = Image.open("app/object_detection/output/test.jpg")
+        img = Image.open("/app/object_detection/output/test.jpg")
         st.image(img, caption="prediction")    
 
 if __name__ == "__main__":
